f73d446a6360594333385474893dd69b4f67d588
Ketan <gondaliya900@gmail.com>
2019-04-27 15:57:55 +0530

    multiple number feature added

--- index.js
+++ index.js
@@ -42,36 +42,58 @@ app.get('/coolcash', (req, res) => {
     res.render('index');
 })
 
+
 app.post('/download', (req, res) => {
-    let number = req.body.phno;
-    let isOld = false;
-    if (number !== NaN && number.length == 10) {
+    var sql = "INSERT INTO cooltable (number) VALUES ?";
 
-        function updateDb() {
-            if (!isOld) {
-                var sql = 'INSERT INTO `cooltable`(`number`) VALUES (' + number + ')';
-                con.query(sql, number, function (err, result) {
-                    if (err) throw err;
-                    console.log("1 record inserted");
-                });
-            }
-            res.render('download');
-        }
-
-
-        var sqlget = 'SELECT `number` FROM `cooltable`'
-        con.query(sqlget, function (err, result) {
-            result.forEach(element => {
-                if (number == element.number) {
-                    isOld = true;
-                }
-            });
-            updateDb();
-        })
-    } else {
-        res.redirect('/coolcash');
+    var cpyArr = [];
+    for (i = 0; i < req.body.number.length; i++) {
+        cpyArr[i] = [req.body.number[i]];
     }
+    console.log(cpyArr)
+    // console.log(cpyArr)
+
+    const tryOne = async () => {
+
+        await cpyArr.forEach(numberArr => {
+            console.log(numberArr[0])
+            if (numberArr[0] == NaN || numberArr[0].length != 10) {
+                cpyArr.splice(cpyArr.indexOf(numberArr), 1);
+                console.log('slice1')
+            }
+            else {
+                var sqlget = 'SELECT `number` FROM `cooltable`'
+                con.query(sqlget, function (err, result) {
+                    result.forEach(element => {
+                        if (numberArr[0] == element.number) {
+                            cpyArr.splice(cpyArr.indexOf(numberArr), 1);
+                            console.log('slice2')
+                        }
+                    });
+                })
+            }
+        })
+
+        console.log(cpyArr)
+        await con.query(sql, [cpyArr], function (err, result) {
+            console.log(cpyArr)
+            if (err) throw err;
+            console.log("Number of records inserted: " + result.affectedRows);
+            if (result.affectedRows >= 1) {
+                res.render('download');
+            }
+            else {
+                res.redirect('/coolcash');
+
+            }
+        });
+
+    }
+    tryOne();
+
 })
+
+
 app.post('/downloadFun', (req, res) => {
 
 
@@ -87,7 +109,7 @@ app.post('/downloadFun', (req, res) => {
         vCard.saveToFile('./cooloffer.vcf');
         res.sendFile(__dirname + '/cooloffer.vcf');
     });
-
+    
 })
 
 app.get('/download', (req, res) => {
